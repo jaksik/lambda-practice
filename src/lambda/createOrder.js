2d2be307939dab5b89935437fe7c88b0bc7195b9
connor <connor.jaksik@gmail.com>
2019-08-25 14:56:57 -0600

    fully functional lambda stripe customer checkout with shipping info

--- src/lambda/createOrder.js
+++ src/lambda/createOrder.js
@@ -1,42 +1,23 @@
 const stripe = require('stripe')('sk_test_DiIrhluODliOuo2J5EAxaGxj00DdFVPCoZ');
 
 module.exports.handler = async (event, context, callback) => {
-  console.log("event: ", event.body)
-  console.log("queryStringParameters", event.queryStringParameters)
-  stripe.orders.create({
+  const requestBody = JSON.parse(event.body)
+
+  const order = await stripe.orders.create({
     currency: 'usd',
     email: 'jenny.rosen@example.com',
-    items: [
-      {
-        type: 'sku',
-        parent: 'sku_FgJVldqsyrIzqQ',
-        quantity: 2,
-      },
-    ],
-    shipping: {
-      name: 'Jenny Rosen',
-      address: {
-        line1: '1234 Main Street',
-        city: 'San Francisco',
-        state: 'CA',
-        postal_code: '94111',
-        country: 'US',
-      },
-    },
+    items: requestBody.items,
+    shipping: requestBody.shipping,
   });
 
   stripe.tokens.create({
-    card: {
-      number: '4242424242424242',
-      exp_month: 12,
-      exp_year: 2020,
-      cvc: '123'
-    }
+    card: requestBody.token.card
   }, function(err, token) {
-      console.log("Token")
+      if (err) {
+        console.log("======= ERROR: ", err)
+      }
       // asynchronously called
-
-      stripe.orders.pay('or_1FBOODHLQDCw6eFcn9DC2RDC', {
+      stripe.orders.pay(order.id, {
           source: token.id, // obtained with Stripe.js
       }, function(err, order) {
           // asynchronously called
@@ -47,10 +28,11 @@ module.exports.handler = async (event, context, callback) => {
         // return null to show no errors
         statusCode: 200, // http status code
         body: JSON.stringify({
+          err: err,
           token: token,
+          order: order,
           msg: "Hello, World! " + Math.round(Math.random() * 10),
         }),
       })  
-    }
-  );   
+    });   
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import StripeCheckout from "react-stripe-checkout";
 
 const IndexPage = () => {
 
@@ -14,17 +15,40 @@ const IndexPage = () => {
       body: JSON.stringify({
         order: {
           currency: 'usd'
-        }
+        },
+        token: {
+          card: {
+            number: '4242424242424242',
+            exp_month: 12,
+            exp_year: 2020,
+            cvc: '123'
+          }
+        },
+        shipping: {
+          name: 'Jenny Rosen',
+          address: {
+            line1: '1234 Main Street',
+            city: 'San Francisco',
+            state: 'CA',
+            postal_code: '94111',
+            country: 'US',
+          },
+        },
+        items: [
+          {
+            type: 'sku',
+            parent: 'sku_FgJVldqsyrIzqQ',
+            quantity: 2,
+          },
+        ],
       })
     })
     .then(call => call.json())
     .then(console.log)
   }
 
-
 token();
   
-
   return (
     <Layout>
       <SEO title="Home" />
@@ -34,6 +58,12 @@ token();
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
+
+      <form>
+          <input type="text"/>
+          <button type="submit">Pay</button>
+          
+      </form>
       <Link to="/page-2/">Go to page 2</Link>
       
     </Layout>
