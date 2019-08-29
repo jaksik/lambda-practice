5c64f8ae01ec76dc29e39c73ed5eb1f985ddfeaa
connor <connor.jaksik@gmail.com>
2019-08-29 11:56:13 -0600

    added import fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import fetch from "node-fetch";
 
 const IndexPage = () => {
 
