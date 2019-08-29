88278943e5800ee97e7ee6b375942c4c34de5618
connor <connor.jaksik@gmail.com>
2019-08-29 11:59:26 -0600

    commiting

--- src/lambda/shipping.js
+++ src/lambda/shipping.js
@@ -1,35 +1,35 @@
-function errorResponse(err, callback) {
-  const response = {
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    statusCode: 500,
-    body: JSON.stringify({
-      error: err
-    })
-  }
+// function errorResponse(err, callback) {
+//   const response = {
+//     headers: {
+//       'Access-Control-Allow-Origin': '*'
+//     },
+//     statusCode: 500,
+//     body: JSON.stringify({
+//       error: err
+//     })
+//   }
 
-  if (typeof callback === 'function') {
-    callback(null, response)
-  }
-}
+//   if (typeof callback === 'function') {
+//     callback(null, response)
+//   }
+// }
 
-export function handler(event, context, callback) {
-  console.log("queryStringParameters", event.queryStringParameters)
+// export function handler(event, context, callback) {
+//   console.log("queryStringParameters", event.queryStringParameters)
 
-  try {
-    fetch("https://www.googleapis.com/books/v1/volumes?q=colorado", {
-      //     method: 'GET',
-      //     mode: 'no-cors',
-      //   //   body: JSON.stringify({
-      //   //     order: {
-      //   //       currency: 'usd'
-      //   //     },
-      //   //   })
+//   try {
+//     fetch("https://www.googleapis.com/books/v1/volumes?q=colorado", {
+//       //     method: 'GET',
+//       //     mode: 'no-cors',
+//       //   //   body: JSON.stringify({
+//       //   //     order: {
+//       //   //       currency: 'usd'
+//       //   //     },
+//       //   //   })
       
-      }).then(response => response.json()) // parses JSON response into native JavaScript objects 
-        .then(console.log)
-  } catch (e) {
-    errorResponse(e, callback)
-}
-}
+//       }).then(response => response.json()) // parses JSON response into native JavaScript objects 
+//         .then(console.log)
+//   } catch (e) {
+//     errorResponse(e, callback)
+// }
+// }
